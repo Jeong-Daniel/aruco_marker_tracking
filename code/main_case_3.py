272667e12504536043ae91fb7acf5b4d445d0cbd
Jeong_daehyeon <85277660+Jeong-Daniel@users.noreply.github.com>
2021-07-26 23:13:00 +0900

    Update main_case_3.py

--- code/main_case_3.py
+++ code/main_case_3.py
@@ -8,6 +8,8 @@ import cv2
 import cv2.aruco as aruco
 import numpy as np
 from math import pi, sqrt, atan2, asin
+import matplotlib.pyplot as plt
+#from scipy.spatial.transform import Rotation
 
 # 사용해려는 행렬이 유효한지 확인
 def isRotationMatrix(R) :
@@ -20,8 +22,15 @@ def isRotationMatrix(R) :
 #t벡터와 r벡터를 문자열 변환함수로 변환한다음 출력하기 위한 함수
 def vtos(rvecs):
     temp = np.round_(rvecs[0].reshape((3, 1)),5)
+    temp*=100
     Rstr = 'X:'+str(temp[0])+'  Y:'+str(temp[1])+'  Z:'+str(temp[2])
     return Rstr
+
+def g_v(rvecs):
+    temp = np.round_(rvecs[0].reshape((3, 1)),5)
+    temp.tolist()
+    temp=[temp[0][0],temp[1][0],temp[2][0]]
+    return temp
 
 #오일러 각도에 대한 회전 행렬 계산
 #오일러 각도에 대해서 x와 z를 교환함
@@ -37,6 +46,11 @@ def rotationMatrixToEulerAngles(R) :
         x = 180*atan2(-R[1,2], R[1,1])/pi
         y = 180*atan2(-R[2,0], sy)/pi
         z = 0
+    
+    if x < 0:
+        x+=180
+    else:
+        x-=180
         
     x=round(x,5)
     y=round(y,5)
@@ -44,6 +58,59 @@ def rotationMatrixToEulerAngles(R) :
     
     Rstr = ['Pitch:'+str(x),'Yaw: '+str(y),'Roll: '+str(z)]
     return Rstr
+
+def rotationMatrixToEulerAngles_C(R) :
+    assert(isRotationMatrix(R))
+    sy = sqrt(R[0,0] * R[0,0] +  R[1,0] * R[1,0])
+    singular = sy < 1e-6
+    if  not singular :
+        x = 180*atan2(R[2,1] , R[2,2])/pi
+        y = 180*atan2(-R[2,0], sy)/pi
+        z = 180*atan2(R[1,0], R[0,0])/pi
+    else :
+        x = 180*atan2(-R[1,2], R[1,1])/pi
+        y = 180*atan2(-R[2,0], sy)/pi
+        z = 0
+
+    x=round(x,5)
+    y=round(y,5)
+    z=round(z,5)
+    
+    Rstr = ['C_Pitch:'+str(x),'C_Yaw: '+str(y),'C_Roll: '+str(z)]
+    return Rstr
+
+def graph(R):
+    assert(isRotationMatrix(R))
+    sy = sqrt(R[0,0] * R[0,0] +  R[1,0] * R[1,0])
+    singular = sy < 1e-6
+    if  not singular :
+        x = 180*atan2(R[2,1] , R[2,2])/pi
+        y = 180*atan2(-R[2,0], sy)/pi
+        z = 180*atan2(R[1,0], R[0,0])/pi
+    else :
+        x = 180*atan2(-R[1,2], R[1,1])/pi
+        y = 180*atan2(-R[2,0], sy)/pi
+        z = 0
+    if x < 0:
+        x+=180
+    else:
+        x-=180
+    return [x,y,z]
+
+"""
+def eulerAnglesToRotationMatrix(euler):
+     R_x = np.array([[                1,                 0,                 0],
+                     [                0,  np.cos(euler[0]), -np.sin(euler[0])],
+                     [                0,  np.sin(euler[0]),  np.cos(euler[0])]])
+     R_y = np.array([[ np.cos(euler[1]),                 0,  np.sin(euler[1])],
+                     [ 0,                                1,                 0],
+                     [-np.sin(euler[1]),                 0,  np.cos(euler[1])]])
+     R_z = np.array([[ np.cos(euler[2]), -np.sin(euler[2]),                 0],
+                     [ np.sin(euler[2]),  np.cos(euler[2]),                 0],
+                     [                0,                 0,                 1]])
+     R = np.dot(R_z, np.dot(R_y, R_x))
+     return R
+"""
 
 #비디오불러오기, 640x480으로 설정
 cap = cv2.VideoCapture(0)
@@ -54,7 +121,7 @@ dictionary = aruco.getPredefinedDictionary(aruco.DICT_6X6_250)
 #위에서부터 마커사이즈(단위m),왜곡계수,카메라포즈
 marker_size=0.15
 #distCoeffs=np.array([k1, k2, p1, p2, k3])
-"""서피스랩탑4
+#서피스랩탑4
 distCoeffs=np.array([0.015701, 0.190824, 0.001675, -0.003972, 0])
 K = np.array([[623.798362, 0, 320],
               [0, 623.798362, 240],
@@ -65,8 +132,11 @@ distCoeffs=np.array([0.010161, -0.090434, 0.007061, 0.002154, 0])
 K = np.array([[572.434936, 0, 320],
               [0, 572.434936, 240],
               [0, 0, 1]], dtype=np.float32)
+"""
 #출력에 사용할 폰트
 font=cv2.FONT_HERSHEY_SIMPLEX
+trans = []
+angle = []
 
 while True:
     ret, frame = cap.read()
@@ -76,7 +146,7 @@ while True:
     corners, ids, _ = aruco.detectMarkers(gray, dictionary)
     #주어진 파라메타를 통해서 회전벡터와 트랜잭션벡터
     rvecs, tvecs, __ = aruco.estimatePoseSingleMarkers(corners, marker_size, K, distCoeffs)
-
+    
     #코너가 한개이상 나올때(마커가 감지될때)
     if len(corners) > 0:
         #마커 그리기
@@ -85,11 +155,20 @@ while True:
         aruco.drawAxis(frame, K, distCoeffs, rvecs, tvecs, 0.1)
         #영상위에 글 출력
         cv2.putText(frame, vtos(tvecs), (0, 100), font, 1, (139, 0, 255))
+        trans.append(g_v(tvecs))
         R = cv2.Rodrigues(rvecs)[0]
-        Rstr = rotationMatrixToEulerAngles(R)
-        cv2.putText(frame, Rstr[0], (0, 150), font, 1, (0, 255, 0))
-        cv2.putText(frame, Rstr[1], (0, 200), font, 1, (255, 0, 0))
-        cv2.putText(frame, Rstr[2], (0, 250), font, 1, (0, 0, 255))
+        R_T = R.T
+
+        Rstr_A = rotationMatrixToEulerAngles(R)
+        cv2.putText(frame, Rstr_A[0], (0, 150), font, 1, (0, 255, 0))
+        cv2.putText(frame, Rstr_A[1], (0, 200), font, 1, (255, 0, 0))
+        cv2.putText(frame, Rstr_A[2], (0, 250), font, 1, (0, 0, 255))
+        angle.append(graph(R))
+        
+        Rstr_C = rotationMatrixToEulerAngles_C(R_T)
+        cv2.putText(frame, Rstr_C[0], (0, 300), font, 1, (0, 255, 0))
+        cv2.putText(frame, Rstr_C[1], (0, 350), font, 1, (255, 0, 0))
+        cv2.putText(frame, Rstr_C[2], (0, 400), font, 1, (0, 0, 255))
 
     #영상출력
     cv2.imshow('frame',frame)
@@ -97,3 +176,6 @@ while True:
         break
 cap.release()
 cv2.destroyAllWindows()
+
+#plt.title("angle"); plt.plot(angle); plt.show()
+#plt.title("trans"); plt.plot(trans); plt.show()
