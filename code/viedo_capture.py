26ec1da540b0b6075d58faa824734ddd9cdfbae1
Jeong_daehyeon <85277660+Jeong-Daniel@users.noreply.github.com>
2021-08-02 23:06:43 +0900

    Update viedo_capture.py

--- code/viedo_capture.py
+++ code/viedo_capture.py
@@ -5,7 +5,21 @@ Created on Mon Aug  2 22:09:54 2021
 @author: dhjun
 """
 
+#!/opt/local/bin/python
 # -*- coding: utf-8 -*-
+import picamera                   # 라즈베리파이 카메라용 라이브러리 로드
+import time                       # sleep을 사용하기 위한 라이브러리
+import datetime
+path = "C:\\Users\\dhjun\\Documents\\"
+now = str(datetime.datetime.now().strftime("%d_%H-%M-%S"))
+cam = picamera.PiCamera()         # 라즈베리파이 카메라 열기
+cam.start_preview()               # 카메라 Preview 시작
+time.sleep(5)                    # 10초간 대기
+cam.stop_preview()                # 카메라 Preview 정지
+cam.capture(paht+now+'png') # 카메라 캡쳐 (image.jpg로 저장)
+
+# -*- coding: utf-8 -*-
+"""
 import cv2
 import datetime
 
@@ -30,3 +44,4 @@ def capture(camid = CAM_ID):
     
 if __name__ == '__main__':
     capture()
+"""
